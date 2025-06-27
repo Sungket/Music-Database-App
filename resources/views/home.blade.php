246a0eb25192617d68411f67372573ccf72e39d4
Sungket <sungket_rhymes@hotmail.com>
2025-06-27 14:27:10 +0100

    added the home.blade.php file

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=" />
+    <title>Home Page</title> 
+</head>
+<body>
+    <h1>Hello Laravel World!</h1>
+</body>
+
+</html>
