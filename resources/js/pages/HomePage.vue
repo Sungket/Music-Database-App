63e0f6953ddb73b2a964b48590ba386ef2ee8b7d
Sungket <sungket_rhymes@hotmail.com>
2025-07-03 13:51:28 +0100

    Added HomePage.vue file

---
+++ resources/js/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<script setup lang="js">
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Home Page">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+
+    <div>
+        <h1>Hello Vue and Laravel World!</h1>
+    </div>
+</template>
+
